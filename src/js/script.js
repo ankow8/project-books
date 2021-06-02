51711252a837fc4d6b73222b36e731840f0c3145
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-06-03 01:32:01 +0200

    Add favoriteBooks function

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,38 @@
   'use strict';
 
   const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-  const bookList = document.querySelector('.books-list');
+  const booksList = document.querySelector('.books-list');
+  //const booksImage = document.querySelectorAll('.book__image');
+  //const booksImage = booksList.querySelectorAll('book__image');
   //console.log(bookList);
 
   function render(){
     for(let book of dataSource.books){
-      console.log(book);
+      //console.log(book);
       const generatedHTML = template(book);
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
-      bookList.appendChild(generatedDOM);
+      booksList.appendChild(generatedDOM);
     }
   }
   render();
+
+  const favoriteBooks = [];
+
+  function initActions(){
+    const booksImage = booksList.querySelectorAll('.book__image');
+    //console.log(booksImage);
+    for(let bookImage of booksImage){
+      console.log(bookImage);
+      bookImage.addEventListener('dblclick', function(event){
+        //console.log('książka się kliknęła');
+        event.preventDefault();
+        bookImage.classList.add('favorite');
+        const bookId = bookImage.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+      });
+    }
+  }
+  initActions();
+  console.log(favoriteBooks);
+
 }
