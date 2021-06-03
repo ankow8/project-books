b3c7463ca7be613bca0dbbc3dfb0ce511a8f84ba
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-06-03 22:48:10 +0200

    Add function remove from favorites

--- src/js/script.js
+++ src/js/script.js
@@ -19,19 +19,28 @@
 
   const favoriteBooks = [];
 
-  function initActions(){
+  function initActions(clickedImage){
     const booksImage = booksList.querySelectorAll('.book__image');
     //console.log(booksImage);
     for(let bookImage of booksImage){
-      console.log(bookImage);
+      //console.log(bookImage);
       bookImage.addEventListener('dblclick', function(event){
         //console.log('książka się kliknęła');
         event.preventDefault();
-        bookImage.classList.add('favorite');
-        const bookId = bookImage.getAttribute('data-id');
-        favoriteBooks.push(bookId);
+
+        if(bookImage.classList.contains('favorite')){
+          bookImage.classList.remove('favorite');
+          const removedBookId = favoriteBooks.indexOf(clickedImage);  //mam wrazenie ze nie usuwa to id co trzeba
+          favoriteBooks.splice(removedBookId, 1);
+        } else {
+          bookImage.classList.add('favorite');
+          const bookId = bookImage.getAttribute('data-id');
+          favoriteBooks.push(bookId);
+        }
+
       });
     }
+    //console.log(booksImage);
   }
   initActions();
   console.log(favoriteBooks);
